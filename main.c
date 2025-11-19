a7bfcbb332ae04562903bd68d4a3794daf79fa24
Magnus Bødker Villumsen <magnus@boedkervillumsen.com>
2025-11-19 21:09:43 +0100

    translated comments to english. Added prints so it output what the algorithm does for learning output. Added a function to print bfs definition.

--- main.c
+++ main.c
@@ -2,22 +2,34 @@
 #include "bfs_matrix.h"
 
 int main(void) {
-    int n = 5;
-    int adj[5][5] = {
-        {0,1,0,0,0},
-         {1,0,1,0,0},
-         {0,1,0,0,0},
-         {0,0,0,0,1},
-         {0,0,0,1,0}
+    int n = 6;
+    int adj[6][6] = {
+        {0,1,0,0,0,0}, // 0
+        {1,0,1,0,1,0}, // 1
+        {0,1,0,1,0,1}, // 2
+        {0,0,1,0,0,0}, // 3
+        {0,1,0,0,0,0}, // 4
+        {0,0,1,0,0,0}  // 5
     };
 
+    print_definition_of_BFS();
+    printf("Now lets run the algorithm \n");
     int bfs[6];
     int visited_count = bfs_matrix(n, adj, 0, bfs);
 
-    printf("BFS rækkefølge: ");
-    for (int i = 0; i < visited_count; i++)
-        printf("%d ", bfs[i]);
-    printf("\n");
+    //following is important for print the BFS matrix correctly
+    int no_comma_on_first = 1;
+    printf("} Final BFS list is: {");
+
+    for (int i = 0; i < visited_count; i++) {
+        if (!no_comma_on_first) {
+            printf(", ");
+        }
+        printf("%d", bfs[i]);
+        no_comma_on_first = 0;
+    }
+    printf("}\n");
+    // ends here
 
     if (visited_count == n)
         printf("Grafen er sammenhængende fra node 0\n");
