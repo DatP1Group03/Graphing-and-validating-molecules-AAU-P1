2fd2f4587ac60869344c9dd94e803b58e919e202
Magnus Bødker Villumsen <magnus@boedkervillumsen.com>
2025-12-01 20:24:14 +0100

    Lavet scroll funktion til adjacency matrix, stability, samt dfs og bfs. Jeg har flyttet bfs og dfs over i gui library så alt er helt samlet.

--- Databehandling/bfs_matrix.h
+++ Databehandling/bfs_matrix.h
@@ -13,7 +13,4 @@ int bfs_matrix(int n, const int adj[n][n], int src, int bfs[]);
 // Tekstlig forklaring i terminal (din oprindelige)
 void print_definition_of_BFS(void);
 
-//GUI bfs matrix istedet for printf
-int bfs_matrix_drawtext_ONLYFORUSEINRAYGUI(int n, const int adj[n][n], int src, int bfs[], int lineheight);
-
 #endif // PROJEKT_KODE_BFS_MATRIX_H
