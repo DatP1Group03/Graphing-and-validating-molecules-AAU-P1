01e7d32b64f2676c912d5ade7649addb89f2e14e
Magnus Bødker Villumsen <magnus@boedkervillumsen.com>
2025-12-03 13:45:54 +0100

    Lavet toxicphore library, og fået den matche symboler - mangler pt bindinger

---
+++ toxicphore/toxicphore.c
@@ -0,0 +1,147 @@
+#include "toxicphore.h"
+#include <ctype.h>
+#include <stdio.h>
+#include "Adjacency_matrix.h"
+int fill_atom_symbols_from_smile( const char *smiles, char atom_symbol[], int max_atoms){
+	int count =0; 
+
+	for (int i=0; smiles[i] != '\0' && count < max_atoms; i++){
+      		char c = smiles[i]; 
+      		
+      		if (isupper(c)){
+      			atom_symbol[count++] = c; 
+		}
+	}
+	return count; 
+}
+
+
+int dfs_toxicphore(int t, int n_tox, const int adj_tox[n_tox][n_tox], char tox_symbol[n_tox], int n_main, const int adj_main[n_main][n_main], char main_symbol[n_main], int mapping_tox_to_main[n_tox], int used_main[n_main], int count) {
+	// vi forsøger at finde f: noder i tox --> noder i main 
+	// f er injektiv, ingen to noder i tox må pege på samme node i main
+	// Dette betyder følgende
+	// 	atomtype skal matche altså tox_symbol(node) == main_symbol(node) 
+	// 	hvis der er en kant mellem to noder i tox så skal der også være en kant i main. altså hvis 
+	// 	t er en node i tox, og er nabo til u. Så skal i main være f(t)-f(u) være naboer. 
+	// Som det første i denne funktion skal vi gå til en nabo fra vores startnode t. og formålet er at for hver nabo i tox skal vi finde en passende nabo i main. 
+	// Så det første vi skal det er at finde en nabo til vores startnode t.  D
+	//
+	if (count == n_tox) {
+		printf("Har matchet alle %d tox-noder! \n", n_tox); 
+		return count; 
+	}
+
+	for (int u = 0; u < n_tox; u++){
+		printf("kører loop %d \n", u); 
+      	if (adj_tox[t][u] >= 1){
+		printf("og vi har fundet et match mellem %d %d \n", t, u); 
+      		// hvis vi allerede har mappet denne node så skal der være en kant mellem f(t) og f(u) så dette skal vi tjekke
+		if (mapping_tox_to_main[u] != -1){
+      			// hvis vi har mappet så kan vi fortsætte med dfs fra noden u. 
+			if (adj_main[mapping_tox_to_main[t]][mapping_tox_to_main[u]] >= 1){
+				printf("Allerede matchet! \n"); 
+				int new_count = dfs_toxicphore(u, n_tox, adj_tox, tox_symbol, n_main, adj_main, main_symbol, mapping_tox_to_main, used_main, count);
+
+				if (new_count != 0){ return new_count;} else {return 0;}
+      				} else {
+					printf("fandet ingen match \n"); 
+      					return 0;
+      				} 
+		} else {// ellers har vi inne fundet et match endnu, og så skal vi finde en kandidat der kan være et match 
+      			printf("Vi har ikke et match og derfor skal vi søge! \n"); 
+			for (int j = 0; j < n_main; j++){
+				printf("Vi søger mellem %d %d om der er et match! \n", mapping_tox_to_main[t], j);  
+      				if (adj_main[mapping_tox_to_main[t]][j] >= 1 && used_main[j] == 0 && tox_symbol[u] == main_symbol[j] ){
+      						printf("fandt et match! \n");
+						used_main[j] = 1; 
+						mapping_tox_to_main[u] = j; 
+						count++;
+						int new_count = dfs_toxicphore(u, n_tox,adj_tox, tox_symbol, n_main, adj_main, main_symbol, mapping_tox_to_main, used_main, count);
+						if (new_count != 0) {
+							return new_count;   // succes, bare boble op
+							 }
+						// backtrack hvis ikke man kan fortsætte :
+						used_main[j] = 0;
+						mapping_tox_to_main[u] = -1;
+						count--;
+					}
+      		}
+			// hvis ingenj virker 
+			return 0; }
+      	}
+	}
+	printf("og vi returner count! \n"); 
+	return count; 
+}
+
+int toxicphore_function(char *smile, char *toxicphore) {
+	int n_main = get_atom_count(smile);
+	int adj_main[n_main][n_main];
+	create_adjacency_matrix(smile, n_main,adj_main); 
+	char main_symbol[n_main]; 
+	int main_count_check = fill_atom_symbols_from_smile(smile, main_symbol, n_main); 
+
+	if (main_count_check != n_main){ 
+		printf("Something went wrong in main_symbol! \n"); 
+		return -1; 
+	}
+
+
+	int n_tox = get_atom_count(toxicphore); 
+	int adj_tox[n_tox][n_tox];
+	create_adjacency_matrix(toxicphore, n_tox, adj_tox);
+	char tox_symbol[n_tox]; 
+	int tox_count_check = fill_atom_symbols_from_smile(toxicphore, tox_symbol, n_tox);
+
+	if (tox_count_check != n_tox){
+		printf("Something went wrong in tox symbol! \n"); 
+		return -1; 
+	}
+
+	int find_toxic = find_toxicphore_in_main(n_tox, adj_tox, tox_symbol, n_main, adj_main, main_symbol);
+	
+	if (find_toxic == 1){return 1;} else {return 0;}
+}
+
+int find_toxicphore_in_main(
+    int n_tox, const int adj_tox[n_tox][n_tox], char tox_symbol[n_tox],
+    int n_main, const int adj_main[n_main][n_main], char main_symbol[n_main]
+) {
+    int mapping_tox_to_main[n_tox];
+    int used_main[n_main];
+
+    // init
+    for (int i = 0; i < n_tox; i++) mapping_tox_to_main[i] = -1;
+    for (int j = 0; j < n_main; j++) used_main[j] = 0;
+
+    // prøv alle main-noder som kandidat for tox-node 0
+    for (int j = 0; j < n_main; j++) {
+        if (main_symbol[j] == tox_symbol[0]) {
+            printf("Prøver startmatch tox 0 (%c) -> main %d (%c)\n",
+                   tox_symbol[0], j, main_symbol[j]);
+
+            mapping_tox_to_main[0] = j;
+            used_main[j] = 1;
+
+            int result = dfs_toxicphore(
+                0,
+                n_tox, adj_tox, tox_symbol,
+                n_main, adj_main, main_symbol,
+                mapping_tox_to_main, used_main,
+                1   // count = 1, fordi vi allerede har matchet node 0
+            );
+
+            if (result == n_tox) {
+                printf("Fandt fuldt toxicphore-match!\n");
+                // her kunne du fx printe mappingen også
+                return 1;
+            }
+
+            // backtrack start-valget
+            mapping_tox_to_main[0] = -1;
+            used_main[j] = 0;
+        }
+    }
+
+    return 0; // ingen match
+}
