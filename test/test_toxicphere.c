37aa2c5bd14132f5f1e8350cd194d8fef180674c
Magnus Bødker Villumsen <magnus@boedkervillumsen.com>
2025-12-03 22:28:17 +0100

    toxic with sidechain fejler - forsøger at finde fejl

--- test/test_toxicphere.c
+++ test/test_toxicphere.c
@@ -56,7 +56,6 @@ TEST_CASE(multiple_matches,{
 	int test = toxicphore_function(smile, toxicphore); 
 
 
-
 	  //test 
 	CHECK_EQ_INT(1, test);  
 })
@@ -76,4 +75,151 @@ TEST_CASE(sidechains,{
 	CHECK_EQ_INT(1, test);  
 })
 
-MAIN_RUN_TESTS(Propanal_dobbeltbond, NC, CO, multiple_matches, sidechains)
+TEST_CASE(sidechains_bigger,{
+	  // arrange
+	  char smile[] = "CCN(CCOCC)NCCN"; 
+	char toxicphore[] ="NCCN"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(1, test);  
+})
+
+
+TEST_CASE(no_match,{
+	  // arrange
+	  char smile[] = "CCOCC"; 
+	char toxicphore[] ="CN"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(0, test);  
+})
+
+
+TEST_CASE(atom_are_present_but_no_connection,{
+	  // arrange
+	  char smile[] = "NCCO"; 
+	char toxicphore[] ="NCO"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(0, test);  
+})
+
+
+TEST_CASE(mixed_atomtypes,{
+	  // arrange
+	  char smile[] = "CCONCC"; 
+	char toxicphore[] ="CON"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(1, test);  
+})
+
+
+TEST_CASE(sidechain_but_no_connection_to_neighbour,{
+	  // arrange
+	  char smile[] = "CC(N)CC"; 
+	char toxicphore[] ="CNC"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(0, test);  
+})
+
+
+TEST_CASE(overlapping_matches,{
+	  // arrange
+	  char smile[] = "COCOC"; 
+	char toxicphore[] ="COC"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(1, test);  
+})
+
+TEST_CASE(ring_structure_match,{
+	  // arrange
+	  char smile[] = "C1CCCCC1"; 
+	char toxicphore[] ="CCC"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(1, test);  
+})
+
+
+TEST_CASE(more_atom_in_tox,{
+	  // arrange
+	  char smile[] = "CC"; 
+	char toxicphore[] ="CCC"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(0, test);  
+})
+
+
+TEST_CASE(toxic_with_sidechain,{
+	  // arrange
+	printf("Toxic with sidechain \n"); 
+	  char smile[] = "CC(C)CC"; 
+	char toxicphore[] ="C(C)C"; 
+
+	  // assert 
+	int test = toxicphore_function(smile, toxicphore); 
+
+
+
+	  //test 
+	CHECK_EQ_INT(1, test);  
+})
+
+MAIN_RUN_TESTS(Propanal_dobbeltbond, 
+	       NC, 
+	       CO, 
+	       multiple_matches, 
+	       sidechains, 
+	       sidechains_bigger,
+	       no_match,
+	       atom_are_present_but_no_connection,
+	       mixed_atomtypes,
+	       sidechain_but_no_connection_to_neighbour,
+	       overlapping_matches,
+	       ring_structure_match,
+	       more_atom_in_tox,
+	       toxic_with_sidechain)
